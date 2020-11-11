09ff6765b9ff7659582fa9677ab172a842d68c8d
Thilo Behnke <thilo.behnke@gmx.net>
2020-11-11 23:50:11 +0100

    bugfix/build-issue

--- client/src/app/interpreter/interpreter.module.ts
+++ client/src/app/interpreter/interpreter.module.ts
@@ -6,20 +6,18 @@ import { MatIconModule } from '@angular/material/icon';
 import { HttpClientModule } from '@angular/common/http';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { CommonModule } from '@angular/common';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import {
   DefaultInterpreterService,
   InterpreterService,
 } from './interpreter.service';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule, Routes } from '@angular/router';
 import { AstVisualizerComponent } from './ast/ast-visualizer.component';
 import { MatCardModule } from '@angular/material/card';
 import { InterpreterResultComponent } from './result/interpreter-result.component';
 import { SharedModule } from '../shared/shared.module';
 import { InterpreterInputComponent } from './input/interpreter-input.component';
-import { InterpreterSubmitComponent } from './input/interpreter-submit.component';
 import { InterpreterCommandComponent } from './commands/interpreter-command.component';
 import { MatOptionModule } from '@angular/material/core';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
