707ed6b612b6b311ca62e4d801b59b83d1c0e987
Thilo Behnke <thilo.behnke@gmx.net>
2020-11-11 23:33:13 +0100

    devOps/fix-linting-issues

--- client/src/app/interpreter/interpreter.module.ts
+++ client/src/app/interpreter/interpreter.module.ts
@@ -50,7 +50,6 @@ export const interpreterRoutes: Routes = [
     AstVisualizerComponent,
     InterpreterResultComponent,
     InterpreterInputComponent,
-    InterpreterSubmitComponent,
     InterpreterCommandComponent,
   ],
   providers: [
